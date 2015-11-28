f771b550c34ffd8cfc4fc8b83372e833e423e4d6
saasan <saasan.s2works@hotmail.com>
2015-11-29 00:24:04 +0900

    html_beautifier.rb

---
+++ _source/_plugins/html_beautifier.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'htmlbeautifier'
+
+Jekyll::Hooks.register :documents, :post_render do |page|
+  next if page.output_ext != '.html'
+  page.output = HtmlBeautifier.beautify(page.output)
+end
+
+Jekyll::Hooks.register :pages, :post_render do |page|
+  next if page.ext != '.html'
+  page.output = HtmlBeautifier.beautify(page.output)
+end
