69443c36d182fb333d4845d56ec1b27de0bc1287
saasan <saasan.s2works@hotmail.com>
2015-12-04 21:55:22 +0900

    build-sync

--- build-sync.bat
+++ build-sync.bat
@@ -10,5 +10,7 @@ SET JEKYLL_ENV=production
 CALL jekyll build --source _source --trace
 ENDLOCAL
 
+robocopy _site ..\saasan.github.io * /MIR /XD mobamas-dojo .git .sass-cache /XF .gitattributes .gitignore /XA:SH
+
 POPD
 PAUSE
