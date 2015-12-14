e098a45293c517c600214e980b41dee281d81fd0
saasan <saasan.s2works@hotmail.com>
2015-12-14 19:06:51 +0900

    drafts

---
+++ _source/_drafts/2015-12-07-jekyll-page-excerpt.md
@@ -0,0 +1,6 @@
+---
+layout: page
+title: jekyll-page-excerpt
+date: 2015-12-07 22:27:10 +0900
+category: blog
+---
--- watch.bat
+++ watch.bat
@@ -6,7 +6,7 @@ PUSHD %~dp0
 
 SETLOCAL
 SET RUBYOPT=-EUTF-8
-CALL jekyll build --source _source --trace --watch
+CALL jekyll build --source _source --trace --watch --drafts
 ENDLOCAL
 
 POPD
