1435b0384edba3c1f5158ef284bf2fea5f65ec28
saasan <saasan.s2works@hotmail.com>
2023-04-19 23:32:12 +0900

    No newline at end of file

--- _source/_plugins/html_beautifier.rb
+++ _source/_plugins/html_beautifier.rb
@@ -3,10 +3,10 @@ require 'htmlbeautifier'
 
 Jekyll::Hooks.register :documents, :post_render do |page|
     next if page.output_ext != '.html'
-    page.output = HtmlBeautifier.beautify(page.output)
+    page.output = HtmlBeautifier.beautify(page.output) + "\n"
 end
 
 Jekyll::Hooks.register :pages, :post_render do |page|
     next if page.ext != '.html'
-    page.output = HtmlBeautifier.beautify(page.output)
+    page.output = HtmlBeautifier.beautify(page.output) + "\n"
 end
