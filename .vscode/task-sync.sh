8e0eb1b4096189cff328c70468f9c5524014e722
saasan <saasan.s2works@hotmail.com>
2025-08-23 04:20:25 +0900

    Remove merged.json from Git tracking

--- .vscode/task-sync.sh
+++ .vscode/task-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -av _site/ ../saasan.github.io/ --exclude=.git/ --delete
+rsync -av _site/ ../saasan.github.io/ --exclude=.git/ --exclude=playground/noreal-koma/merged.json --delete
